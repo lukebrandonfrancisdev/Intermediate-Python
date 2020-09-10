c62c534f79eea6ee283b7f4bd3f4e165899d1aa1
Luke Francis <lukebrandonfrancisdev@gmail.com>
2020-09-10 15:29:17 -0400

    Worked with LAMBDA Expressions

---
+++ lambda.py
@@ -0,0 +1,19 @@
+from functools import reduce
+
+"""add10 = lambda x: x + 10
+print(add10(5))
+
+mult = lambda x, y: x * y
+print(mult(2,3))
+
+points2d = [(1, 2), (15, 1), (5, -1), (10, 4)]
+
+points2d_sorted = sorted(points2d, key= lambda x: x[1])
+
+print(points2d)
+print(points2d_sorted)"""
+
+a = [1, 2, 3, 4]
+
+product_a = reduce(lambda x,y: x*y, a)
+print(product_a)
